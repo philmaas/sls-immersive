471fbfbad97cbd9de4da9bda5d31d4d2b775bfcf
Phil <contact@philippmaas.com>
2016-05-05 07:55:31 +0000

    refined legal notice etc

--- js/tweecool.js
+++ js/tweecool.js
@@ -14,7 +14,7 @@
 				username : 'phili_maas',
 				limit : 5,
 				profile_image : false,
-				show_time : false,
+				show_time : true,
 				show_media : true,
                                 show_media_size: 'thumb',  //values: small, large, thumb, medium 
                                 show_actions: false,
